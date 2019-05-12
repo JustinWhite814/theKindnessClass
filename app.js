ec4df4c430fdbd237b2e818faab868e861e18ff8
Justin White <juswhite814@gmail.com>
2019-05-12 13:26:09 -0400

    my first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose")
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + "/public"))
+app.set("view engine", "ejs");
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+app.get('/home/calendar', function(req, res){
+    res.render('calendar');
+});
+app.get('/home/homework', function(req, res){
+    res.render('homework');
+});
+app.get('/home/current', function(req, res){
+    res.render('current');
+});
+app.get('/home/students', function(req, res){
+    res.render('students');
+});
+app.get('/home/classDojo', function(req, res){
+    res.render('classDojo');
+});
+
+
+app.listen(3000, function() {
+    console.log("welcome to the site!");
+});
